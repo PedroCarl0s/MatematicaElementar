846fe891330a5ee5c883a3f59071aa127fd1795a
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-05-13 10:00:22 -0300

    remoção da View c/ testes LaTeX

--- app/src/main/java/elementar/analise/combinatoria/TelaCombinatoria.java
+++ app/src/main/java/elementar/analise/combinatoria/TelaCombinatoria.java
@@ -1,6 +1,8 @@
 package elementar.analise.combinatoria;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,30 +12,53 @@ import io.github.kexanie.library.MathView;
 
 public class TelaCombinatoria extends AppCompatActivity {
 
+    /*
     MathView permutationPlot;
     String n = "10",  p = "5";
 
     String arranjoForm = "Fórmula do Arranjo"
-            + "$$A(n, p) = \\frac{n!} {(n-p)!}$$";
+            + "$$A(n, p) = \\frac{n!} {(n-p)!}, \\ n \\geqslant p$$";
 
     String combinationForm = "Fórmula da Combinação:"
-            + "$$C(n, p) = \\frac{n!} {p! \\ (n-p)!}$$";
-
-
+            + "$$C(n, p) = \\frac{n!} {p! \\ (n-p)!}, \\ n \\geqslant p$$";
 
     String primeiraImpressao = "Resultado" + "$$A(" + n + ", " + p +
             ") = \\frac{" + n + "!" + "} {(" + n + "-" + p + ")!}$$";
+
+
+    public String resultado() {
+
+        int elementos = 10;
+        int posicoes = 5;
+
+        StringBuilder numerador = new StringBuilder();
+
+        // Percorre do valor do elemento até a subtração (n-p)!, para depois simplificar os valores
+        for (int e = elementos; e >= (elementos-posicoes); e--) {
+            numerador.append(Integer.toString(e));
+            numerador.append(".");
+        }
+
+        // Deleta o último caracter (ponto final em excesso)
+        numerador.delete(numerador.length()-1, numerador.length());
+
+        numerador.append("!");
+
+        String fim = numerador.toString();
+
+        return fim;
+    }
+    */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_combinatoria);
 
-
-        permutationPlot = (MathView) findViewById(R.id.resultado_arranjo);
+        //permutationPlot = (MathView) findViewById(R.id.resultado_arranjo);
         //permutationPlot.setText(arranjoForm+ combinationForm);
-        permutationPlot.setText(primeiraImpressao);
-
+        //permutationPlot.setText(arranjoForm + combinationForm + primeiraImpressao);
 
     }
 
