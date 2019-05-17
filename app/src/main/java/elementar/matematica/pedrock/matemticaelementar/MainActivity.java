ded353075d4801e857bf0be26057799d50fc6c6c
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-05-17 01:44:57 -0300

    adição do método estático para esconder o teclado

--- app/src/main/java/elementar/matematica/pedrock/matemticaelementar/MainActivity.java
+++ app/src/main/java/elementar/matematica/pedrock/matemticaelementar/MainActivity.java
@@ -1,12 +1,14 @@
 package elementar.matematica.pedrock.matemticaelementar;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
-import elementar.analise.combinatoria.Fragments.Arranjo;
 import elementar.analise.combinatoria.TelaCombinatoria;
 import elementar.teoria.dos.conjuntos.TelaConjuntos;
 
@@ -28,6 +30,20 @@ public class MainActivity extends AppCompatActivity {
     public void abrirOperacoesConjuntos(View view) {
         startActivity(new Intent(this, TelaConjuntos.class));
         finish();
+    }
+
+
+    // Método para esconder o teclado após clicar em calcular
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager inputMananger = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        // Verificar se a view está não está com foco
+        View currentFocusedView = activity.getCurrentFocus();
+
+        // Se não estiver com foco, então o teclado vai ser escondido
+        if (currentFocusedView != null) {
+            inputMananger.hideSoftInputFromWindow(currentFocusedView.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 
 }
