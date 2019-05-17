72376083baa9d8cee174dab88c58e77fe2b0ce33
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-05-17 01:42:15 -0300

    nada

--- app/src/main/java/elementar/analise/combinatoria/Fragments/Combinacao.java
+++ app/src/main/java/elementar/analise/combinatoria/Fragments/Combinacao.java
@@ -3,10 +3,19 @@ package elementar.analise.combinatoria.Fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import elementar.matematica.pedrock.matemticaelementar.R;
 
@@ -28,6 +37,8 @@ public class Combinacao extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private View view;
 
     private OnFragmentInteractionListener mListener;
 
@@ -56,17 +67,14 @@ public class Combinacao extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_combinacao, container, false);
+        view = inflater.inflate(R.layout.fragment_combinacao, container, false);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
