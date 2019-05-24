74b58d47789bb7b16bdab863760b9127d27b0b15
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-05-23 23:11:25 -0300

    adição da chamada de método para esconder teclado

--- app/src/main/java/elementar/analise/combinatoria/TelaCombinatoria.java
+++ app/src/main/java/elementar/analise/combinatoria/TelaCombinatoria.java
@@ -6,6 +6,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import elementar.analise.combinatoria.Fragments.Arranjo;
 import elementar.analise.combinatoria.Fragments.Combinacao;
@@ -35,17 +37,16 @@ public class TelaCombinatoria extends AppCompatActivity implements Arranjo.OnFra
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
+                MainActivity.hideKeyboard(TelaCombinatoria.this);
                 viewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
             }
         });
 
