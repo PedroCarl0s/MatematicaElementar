85a9b76199d0dd773d45f468fb3227a17f67eaa2
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-05-25 23:35:18 -0300

    adição dos métodos para animação e carregamento da activity TelaCombinatoria

--- app/src/main/java/elementar/matematica/pedrock/matemticaelementar/MainActivity.java
+++ app/src/main/java/elementar/matematica/pedrock/matemticaelementar/MainActivity.java
@@ -4,14 +4,21 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.Bundle;;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 import elementar.analise.combinatoria.TelaCombinatoria;
 import elementar.teoria.dos.conjuntos.TelaConjuntos;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int  DELAY_TIME = 2100;
+    private static LottieAnimationView animationClock;
+    private Thread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +28,47 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void abrirAnaliseCombinatoria(View view) {
-        //Toast.makeText(getApplicationContext(), "Coming Soon", Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(this, TelaCombinatoria.class));
-        finish();
+
+        // Inicia a animação Lottie
+        startAnimationClock(animationClock, "clock.json");
+
+        // Desativa a Activity (impedir clique em outro botão)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+
+        // Inicia a Thread (delay + pré-carregamento da activity)
+        thread = new Thread(runnable);
+        thread.start();
+    }
+
+    public Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+
+            try {
+                Thread.sleep(DELAY_TIME );
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Inicia a Activity TelaCombinatoria
+            try {
+                startActivity(new Intent(MainActivity.this, TelaCombinatoria.class));
+                finish();
+
+            } catch (Exception e) {
+
+            }
+        }
+
+    };
+
+    private void startAnimationClock(LottieAnimationView animationView, String jsonFile) {
+        animationView = findViewById(R.id.animation_clock);
+        animationView.setAnimation(jsonFile);
+        animationView.setProgress(1.0f);
+        animationView.playAnimation();
     }
 
     public void abrirOperacoesConjuntos(View view) {
