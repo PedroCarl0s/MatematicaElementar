06ae461f175db5597e7ac8a9367aa227ce943806
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-05-29 14:30:18 -0300

    remoção de métodos não usados

--- app/src/main/java/elementar/analise/combinatoria/Fragments/Arranjo.java
+++ app/src/main/java/elementar/analise/combinatoria/Fragments/Arranjo.java
@@ -26,23 +26,11 @@ import elementar.matematica.pedrock.matemticaelementar.R;
 import io.github.kexanie.library.MathView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Arranjo.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Arranjo#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Arranjo extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private View view;
     private Handler handler;
@@ -54,7 +42,6 @@ public class Arranjo extends Fragment {
     private static String valorElementos, valorPosicoes;
     private Button button_calcular;
     private boolean jaCalculou = false;
-    private OnFragmentInteractionListener mListener;
 
     private LottieAnimationView animationWrite, animationSwipe;
     private final int id_write = R.id.animation_write, id_swipe = R.id.animation_swipe;
@@ -62,25 +49,6 @@ public class Arranjo extends Fragment {
 
     public Arranjo() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Arranjo.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Arranjo newInstance(String param1, String param2) {
-        Arranjo fragment = new Arranjo();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-
-        return fragment;
     }
 
     @Override
@@ -295,23 +263,6 @@ public class Arranjo extends Fragment {
     }
 
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
@@ -338,25 +289,4 @@ public class Arranjo extends Fragment {
 
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
