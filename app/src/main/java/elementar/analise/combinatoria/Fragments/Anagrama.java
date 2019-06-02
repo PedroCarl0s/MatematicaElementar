435a37514e859b1d7777b89bcfc359993bea8e22
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-06-02 07:11:38 -0300

    adição dos elementos da interface + fórmula do Anagrama

--- app/src/main/java/elementar/analise/combinatoria/Fragments/Anagrama.java
+++ app/src/main/java/elementar/analise/combinatoria/Fragments/Anagrama.java
@@ -9,8 +9,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import elementar.matematica.pedrock.matemticaelementar.R;
+import io.github.kexanie.library.MathView;
 
 
 /**
@@ -18,10 +23,19 @@ import elementar.matematica.pedrock.matemticaelementar.R;
  */
 public class Anagrama extends Fragment {
 
+    private View view;
+
+    private static TextInputLayout txtAnagrama;
+    private static TextInputEditText inputAnagrama;
+
+    private MathView formulaAnagrama, resultadoAnagrama;
+
+    private Button btnCalcular;
 
     public Anagrama() {
         // Required empty public constructor
     }
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,10 +43,39 @@ public class Anagrama extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onResume() {
+        super.onResume();
+
+        init();
+
+        String formulaAnagrama = "$$\\bold{Formula}$$" +
+                "$$\\large {P_{n} \\ ^ {\\alpha, \\ \\beta, \\ \\gamma}} = \\small {\\frac{n!} {\\alpha! \\ \\beta! \\ ... \\ \\gamma!}}$$";
+
+//        String formulaAnagrama = "$$\\text{Ocorrencias} \\begin{cases}" +
+//                "a = & \\text{2 vezes} \\\\" +
+//                "b = & \\text{3 vezes} \\\\" +
+//                "c = & \\text{2 vezes}" +
+//                "\\end{cases}$$";
+            this.formulaAnagrama.setText(formulaAnagrama);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_anagrama, container, false);
+        this.view = inflater.inflate(R.layout.fragment_anagrama, container, false);
+
+        return view;
+    }
+
+    private void init () {
+        this.txtAnagrama = (TextInputLayout) view.findViewById(R.id.txt_anagrama);
+        this.inputAnagrama = (TextInputEditText) view.findViewById(R.id.inputAnagrama);
+
+        this.formulaAnagrama = (MathView) view.findViewById(R.id.formula_anagrama);
+        this.resultadoAnagrama = (MathView) view.findViewById(R.id.resultado_anagrama);
+
+        this.btnCalcular = (Button) view.findViewById(R.id.btn_calcular);
     }
 
 }
