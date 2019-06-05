2474bab88dbc2962bdaa9eb6aafbb77e46826604
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-06-05 17:22:27 -0300

    teste de gerar descrição com chaves

--- app/src/main/java/elementar/analise/combinatoria/latex/GeradorAnagrama.java
+++ app/src/main/java/elementar/analise/combinatoria/latex/GeradorAnagrama.java
@@ -1,13 +1,25 @@
 package elementar.analise.combinatoria.latex;
 
+import android.util.Log;
+
 public class GeradorAnagrama {
 
     public static String gerarFormula() {
         String formulaAnagrama = "$$\\bold{Formula}$$" +
-                "$$\\large {P_{n} \\ ^ {\\alpha, \\ \\beta, \\ \\gamma}} = \\small {\\frac{n!} {\\alpha! \\ \\beta! \\ ... \\ \\gamma!}}$$";
+                "$$\\large {P_{n} \\ ^ {n_1, \\ n_2, \\ ..., \\ n_k}} = \\small {\\frac{n!} {n_1! \\ n_2! \\ ... \\ n_k!}}$$";
 
         return formulaAnagrama;
     }
 
+    public static String gerarDescricaoVariaveis(String palavraInserida) {
+
+        String descricao = "$$\\text{Ocorrencias} \\begin{cases}" +
+                "a = & \\text{2 vezes} \\\\" +
+                "b = & \\text{3 vezes} \\\\" +
+                "c = & \\text{2 vezes}" +
+                "\\end{cases}$$";
+
+        return descricao;
+    }
 
 }
