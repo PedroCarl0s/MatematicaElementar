8a9ca1bb5446bbcb9f79dd22c80fed3ae7c3b532
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-06-06 10:35:47 -0300

    adição dos atributos básicos da classe

--- app/src/main/java/elementar/analise/combinatoria/fragments/Combinacao.java
+++ app/src/main/java/elementar/analise/combinatoria/fragments/Combinacao.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import elementar.matematica.pedrock.matemticaelementar.R;
+import io.github.kexanie.library.MathView;
 
 
 /**
@@ -19,7 +26,21 @@ import elementar.matematica.pedrock.matemticaelementar.R;
  */
 public class Combinacao extends Fragment {
 
-    View view;
+    private View view;
+    private Handler handler;
+
+    private static TextInputLayout inputElementos, inputPosicoes;
+    private static TextInputEditText txtElementos, txtPosicoes;
+
+    private static MathView formulaCombinacao, resultadoCombinacao;
+    private static int valorElementos, valorPosicoes;
+    private Button btnCalcular;
+    private boolean jaCalculou = false;
+
+    private LottieAnimationView animationWrite, animationSwipe;
+    private final int ID_WRITE = R.id.animation_write, ID_SWIPE = R.id.animation_swipe;
+    private final int DELAY_TIME = 750;
+    private static final int ERRO_CONVERSAO = -100000;
 
     public Combinacao() {
         // Required empty public constructor
@@ -39,6 +60,9 @@ public class Combinacao extends Fragment {
         return view;
     }
 
-
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 
 }
