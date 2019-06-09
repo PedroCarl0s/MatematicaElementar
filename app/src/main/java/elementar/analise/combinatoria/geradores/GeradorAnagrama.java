f2067eee7b99df415a4c81b7af3288b04b5e5e27
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-06-09 17:45:59 -0300

    adição da herança para GeradorFormulas

--- app/src/main/java/elementar/analise/combinatoria/geradores/GeradorAnagrama.java
+++ app/src/main/java/elementar/analise/combinatoria/geradores/GeradorAnagrama.java
@@ -1,11 +1,9 @@
-package elementar.analise.combinatoria.latex;
-
-import android.util.Log;
+package elementar.analise.combinatoria.geradores;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class GeradorAnagrama {
+public class GeradorAnagrama extends GeradorFormulas {
 
     private static final String beginCases = "$$\\text{Ocorrencias} \\begin{cases}";
     private static final String EqualEcomInit = "= \\text{";
@@ -13,26 +11,27 @@ public class GeradorAnagrama {
     private static final String fimTextS = " vezes} \\\\";
     private static final String endCases = "\\end{cases}$$";
 
-    public static String gerarFormula() {
+    public String gerarFormula() {
         String formulaAnagrama = "$$\\bold{Formula}$$" +
                 "$$\\large {P_{n} \\ ^ {n_1, \\ n_2, \\ ..., \\ n_k}} = \\small {\\frac{n!} {n_1! \\ n_2! \\ ... \\ n_k!}}$$";
 
         return formulaAnagrama;
     }
 
-    public static String gerarDescricaoVariaveis(HashMap<String,Integer> mapPalavras) {
+    public String gerarDescricaoVariaveis(HashMap<String,Integer> mapPalavras) {
 
         StringBuilder descricao = new StringBuilder();
 
         descricao.append(beginCases);
 
-        for(Map.Entry<String,Integer> entry : mapPalavras.entrySet()){
+        for (Map.Entry<String,Integer> entry : mapPalavras.entrySet()) {
 
             descricao.append(entry.getKey()).append(EqualEcomInit).append(entry.getValue());
 
-            if(entry.getValue() == 1){
+            if (entry.getValue() == 1) {
                 descricao.append(fimText);
-            }else{
+
+            } else {
                 descricao.append(fimTextS);
             }
 
