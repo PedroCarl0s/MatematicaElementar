6747ffb2b89d25aae58054045d76ec46e394fb00
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-06-09 18:27:10 -0300

    refatoramento adição do metodo gerarAplicacaoValores

--- app/src/main/java/elementar/analise/combinatoria/geradores/GeradorAnagrama.java
+++ app/src/main/java/elementar/analise/combinatoria/geradores/GeradorAnagrama.java
@@ -11,6 +11,14 @@ public class GeradorAnagrama extends GeradorFormulas {
     private static final String fimTextS = " vezes} \\\\";
     private static final String endCases = "\\end{cases}$$";
 
+    private final String inicio1 = "$$\\large {P_{";
+    private final String início2 = " \\small {\\frac{";
+    private final String potencia = "} \\ ^ {";
+    private final String separador1 = ", \\ ";
+    private final String separador2 = "\\ ";
+    private final String fechaChaves1 = "}} =";
+    private final String fechaChaves2 = "}}$$";
+
     public String gerarFormula() {
         String formulaAnagrama = "$$\\bold{Formula}$$" +
                 "$$\\large {P_{n} \\ ^ {n_1, \\ n_2, \\ ..., \\ n_k}} = \\small {\\frac{n!} {n_1! \\ n_2! \\ ... \\ n_k!}}$$";
@@ -42,6 +50,42 @@ public class GeradorAnagrama extends GeradorFormulas {
         descricao.append(endCases);
 
         return String.valueOf(descricao);
+    }
+
+    public String gerarAplicacaoValores(HashMap<String, Integer> hashMapLetras, int getTamanhodaPalavra){
+
+        StringBuilder aplicacao = new StringBuilder(inicio1);
+        aplicacao.append(getTamanhodaPalavra).append(potencia);
+
+        for(Map.Entry<String,Integer> entry : hashMapLetras.entrySet()){
+
+            aplicacao.append(entry.getValue()).append(separador1);
+
+        }
+
+        //remover os \\ sobrando
+        aplicacao = aplicacao.replace(aplicacao.length()-3,aplicacao.length()-1,"");
+
+        //remover , do final
+        aplicacao = aplicacao.replace(aplicacao.length()-2,aplicacao.length()-1,"");
+
+        aplicacao.append(fechaChaves1);
+
+        aplicacao.append(início2).append(getTamanhodaPalavra).append("!} {");
+
+        for(Map.Entry<String,Integer> entry : hashMapLetras.entrySet()){
+
+            aplicacao.append(entry.getValue()).append("! ").append(separador2);
+
+        }
+
+        //remover os \\ sobrando
+        aplicacao = aplicacao.replace(aplicacao.length()-2,aplicacao.length()-1,"");
+
+        aplicacao.append(fechaChaves2);
+
+        return aplicacao.toString();
+
     }
 
 }
