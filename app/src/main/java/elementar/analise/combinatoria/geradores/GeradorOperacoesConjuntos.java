2e8ae61712b9136ba9154f98911a33db5fdf333b
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-07-06 18:27:54 -0300

    classe geradora de operações entre conjuntos

---
+++ app/src/main/java/elementar/analise/combinatoria/geradores/GeradorOperacoesConjuntos.java
@@ -0,0 +1,67 @@
+package elementar.analise.combinatoria.geradores;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class GeradorOperacoesConjuntos {
+
+    public GeradorOperacoesConjuntos(){
+
+    }
+
+    //calculor individuais
+
+    private StringBuilder calcularUniao(TextInputLayout inputA, TextInputLayout inputB, TextInputLayout inputC){
+        return new StringBuilder();
+    }
+
+    private StringBuilder calcularIntersecao(TextInputLayout inputA, TextInputLayout inputB){
+        return new StringBuilder();
+    }
+
+    private StringBuilder calcularComplementar(TextInputLayout inputA, TextInputLayout inputB){
+        return new StringBuilder();
+    }
+
+    private StringBuilder calcularDiferencaAB(TextInputLayout inputA, TextInputLayout inputB){
+        return new StringBuilder();
+    }
+
+    private StringBuilder calcularDiferencaBA(TextInputLayout inputA, TextInputLayout inputB){
+        return new StringBuilder();
+    }
+
+    private StringBuilder calcularConjuntoPartes(TextInputLayout inputA, TextInputLayout inputB){
+        return new StringBuilder();
+    }
+
+    //calcular pares
+
+    private StringBuilder calcularUniaoComplementar(TextInputLayout inputA, TextInputLayout inputB, TextInputLayout inputC){
+        return new StringBuilder();
+    }
+
+    private StringBuilder calcularIntersecaoConjPartes(TextInputLayout inputA, TextInputLayout inputB){
+        return new StringBuilder();
+    }
+
+    private StringBuilder calcularDifABDifBA(TextInputLayout inputA, TextInputLayout inputB){
+        return new StringBuilder();
+    }
+
+    private StringBuilder calcularUniaoIntersecao(TextInputLayout inputA, TextInputLayout inputB){
+        return new StringBuilder();
+    }
+
+    private StringBuilder calcularIntersecaoDifAB(TextInputLayout inputA, TextInputLayout inputB){
+        return new StringBuilder();
+    }
+
+    private StringBuilder calcularComplementarConjPartes(TextInputLayout inputA, TextInputLayout inputB){
+        return new StringBuilder();
+    }
+
+    private StringBuilder calcularConjPartesDifBA(TextInputLayout inputA, TextInputLayout inputB){
+        return new StringBuilder();
+    }
+
+}
