93b356bd2b80d13efe6f8ffb4b6d90292ad3a34a
emanoelmlsilva <emanoel.silva@dce.ufpb.br>
2019-07-13 12:18:22 -0300

    criação do Historico

--- app/src/main/java/elementar/analise/combinatoria/fragments/HistoricoFragment.java
+++ app/src/main/java/elementar/analise/combinatoria/fragments/HistoricoFragment.java
@@ -1,16 +1,22 @@
 package elementar.analise.combinatoria.fragments;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import elementar.analise.combinatoria.activitys.TelaConjuntos;
 import elementar.matematica.pedrock.matemticaelementar.R;
 
 public class HistoricoFragment extends Fragment {
@@ -19,6 +25,7 @@ public class HistoricoFragment extends Fragment {
     private ListView historico;
     private ArrayAdapter adapter;
     private String[] array = new String[]{"novo","novo","novo"};
+    private  ImageButton back;
 
     public HistoricoFragment() { }
 
@@ -28,6 +35,21 @@ public class HistoricoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_historico, container, false);
+        back = view.findViewById(R.id.back);
+        teste();
         return view;
     }
+
+    public void teste(){
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TelaConjuntos.fab.show();
+                getActivity().getSupportFragmentManager().popBackStack();
+
+            }
+        });
+    }
+
 }
