a00544b69fa1584bc7ed7899cc1577e54f8d1970
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2019-12-07 13:34:45 -0300

    Gerando todos os cálculos com LaTeX

--- app/src/main/java/elementar/operacoes/conjuntos/GeradorOperacoesConjuntos.java
+++ app/src/main/java/elementar/operacoes/conjuntos/GeradorOperacoesConjuntos.java
@@ -2,10 +2,23 @@ package elementar.operacoes.conjuntos;
 
 import android.util.Log;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 
 public class GeradorOperacoesConjuntos {
 
-    private final String comma = ",";
+    private final int INDEX_ID = 0;
+    private final int INDEX_VALUES = 1;
+
+    private final String COMMA = ",";
+    private final String DELIMITER = "$$";
+    private final String BRACE_LEFT = "\\lbrace";
+    private final String BRACE_RIGHT = "\\rbrace";
+    private final String NEW_LINE = "\\";
+
+    private final String UNIAO_LATEX = "\\cup";
+    private final String INTERSECAO_LATEX = "\\cap";
+    private final String DIFERENCA_LATEX = "-";
 
     public GeradorOperacoesConjuntos(){
 
@@ -20,18 +33,14 @@ public class GeradorOperacoesConjuntos {
             for(int j = i;j < arrayValue.length;j++){
 
                 if(Integer.parseInt(arrayValue[i]) > Integer.parseInt(arrayValue[j])){
-
                     String aux = arrayValue[i];
                     arrayValue[i] = arrayValue[j];
                     arrayValue[j] = aux;
-
                 }
-
             }
         }
 
         return toArrayString(arrayValue);
-
     }
 
     private String toArrayString(String[] values){
@@ -39,7 +48,7 @@ public class GeradorOperacoesConjuntos {
         StringBuilder value = new StringBuilder();
         for(int i = 0;i < values.length;i++){
             value.append(values[i]);
-            if(i < values.length-1) value.append(comma);
+            if(i < values.length-1) value.append(COMMA);
         }
 
         return value.toString();
@@ -56,13 +65,13 @@ public class GeradorOperacoesConjuntos {
     private String removeEquals(String oldString) {
 
         StringBuilder newValue = new StringBuilder();
-        String[] arrayNumber =  oldString.split(comma);
+        String[] arrayNumber =  oldString.split(COMMA);
 
         for (int i = 0; i < arrayNumber.length; i++) {
 
             String compare = arrayNumber[i];
 
-            String[] auxArray = newValue.toString().split(comma);
+            String[] auxArray = newValue.toString().split(COMMA);
 
             int index = findArray(compare,auxArray);
             Log.i("calc","arrayNumber[i]) "+arrayNumber[i]);
@@ -70,7 +79,7 @@ public class GeradorOperacoesConjuntos {
 
                 newValue.append(arrayNumber[i]);
 
-                if(i < arrayNumber.length -1) newValue.append(comma);
+                if(i < arrayNumber.length -1) newValue.append(COMMA);
 
             }
 
@@ -83,7 +92,7 @@ public class GeradorOperacoesConjuntos {
     public String checkComma(String value){
 
         value = value.replaceAll("\\s+","");
-        String[] arrayValueA = value.split(comma);
+        String[] arrayValueA = value.split(COMMA);
         StringBuilder newValue = new StringBuilder();
 
         for(int i = 0; i < arrayValueA.length; i++){
@@ -91,7 +100,7 @@ public class GeradorOperacoesConjuntos {
             if(!arrayValueA[i].equals("")){
                 newValue.append(arrayValueA[i]);
 
-                if(i < arrayValueA.length -1) newValue.append(comma);
+                if(i < arrayValueA.length -1) newValue.append(COMMA);
             }
 
         }
@@ -105,14 +114,14 @@ public class GeradorOperacoesConjuntos {
         inputA = removeEquals(inputA);
         inputB = removeEquals(inputB);
 
-        String[] arrayInputA = inputA.split(comma);
-        String[] arrayInputB = inputB.split(comma);
+        String[] arrayInputA = inputA.split(COMMA);
+        String[] arrayInputB = inputB.split(COMMA);
 
         for(int i = 0;i < arrayInputA.length;i++){
             for(int j = 0;j < arrayInputB.length;j++){
                 if(Integer.parseInt(arrayInputA[i]) == Integer.parseInt(arrayInputB[j])){
                     newValue.append(arrayInputA[i]);
-                    if(i < arrayInputA.length -1) newValue.append(comma);
+                    if(i < arrayInputA.length -1) newValue.append(COMMA);
                     break;
                 }
             }
@@ -142,7 +151,7 @@ public class GeradorOperacoesConjuntos {
         for(int i = 0;i  < arrayValueA.length;i++){
 
             String compare = arrayValueA[i];
-            String[] auxArray = newValue.toString().split(comma);
+            String[] auxArray = newValue.toString().split(COMMA);
 
             int index = findArray(compare,auxArray);
             int indexValueB = findArray(compare,arrayValueB);
@@ -150,7 +159,7 @@ public class GeradorOperacoesConjuntos {
             if((index == -1 && indexValueB == -1) || newValue.length() == 0 &&  indexValueB == -1){
                 newValue.append(compare);
 
-                if(i < arrayValueA.length - 1) newValue.append(comma);
+                if(i < arrayValueA.length - 1) newValue.append(COMMA);
             }
 
         }
@@ -165,12 +174,22 @@ public class GeradorOperacoesConjuntos {
         return newValue.toString();
     }
 
-    //Cálculos individuais
+    //União
     public StringBuilder calcularUniao(String inputA, String inputB){
         String resultado = checkComma(inputA) +","+ checkComma(inputB);
         return new StringBuilder(removeEquals(resultado));
     }
 
+    public String getTitleUniaoLatex() {
+        return "$$\\normalsize \\bold{Uniao}$$";
+    }
+
+    public String getTextUniao() {
+        return "A união dos conjuntos A e B, são todos os valores que estão em A ou B (as duplicatas são removidas)";
+    }
+
+
+    //Interseção
     public StringBuilder calcularIntersecao(String inputA, String inputB){
         StringBuilder newCalculo = new StringBuilder();
 
@@ -182,17 +201,26 @@ public class GeradorOperacoesConjuntos {
         newCalculo.append(findEquals(inputA,inputB));
 
         if(newCalculo.toString().isEmpty()){
-            return new StringBuilder("N/A");
+            return new StringBuilder("");
         }
 
         Log.i("intersecao","valor final "+newCalculo.toString());
         return newCalculo;
     }
 
+    public String getTitleIntersecaoLatex() {
+        return "$$\\normalsize \\bold{Intersecao}$$";
+    }
+    public String getTextIntersecao() {
+        return "A interseção dos conjuntos A e B, são todos os valores que estão em A e B ao mesmo tempo";
+    }
+
+    //Complementar
     public StringBuilder calcularComplementar(String inputA, String inputB, String inputC){
         return new StringBuilder();
     }
 
+    //Diferença A-B
     public StringBuilder calcularDiferencaAB(String inputA, String inputB){
 
         StringBuilder newCalculo = new StringBuilder();
@@ -205,8 +233,17 @@ public class GeradorOperacoesConjuntos {
         return newCalculo;
     }
 
-    public StringBuilder calcularDiferencaBA(String inputB, String inputA){
+    public String getTitleDiferencaABLatex() {
+        return "$$\\normalsize \\bold{Diferenca \\ (A-B)}$$";
+    }
 
+
+    public String getTextDiferencaAB() {
+        return "A diferença de A-B, são todos os valores que estão em A mas não estão em B";
+    }
+
+    //Diferença B-A
+    public StringBuilder calcularDiferencaBA(String inputB, String inputA){
         StringBuilder newCalculo = new StringBuilder();
 
         inputA = checkComma(inputA);
@@ -217,8 +254,53 @@ public class GeradorOperacoesConjuntos {
         return newCalculo;
     }
 
+    public String getTitleDiferancaBALatex() {
+        return "$$\\normalsize \\bold{Diferenca \\ (B-A)}$$";
+    }
+
+    public String getTextDiferencaBA() {
+        return "A diferença de B-A, são todos os valores que estão em B mas não estão em A";
+    }
+
+    // Conjunto das Partes
     public StringBuilder calcularConjuntoPartes(String inputA, String inputB){
         return new StringBuilder();
+    }
+
+
+    // Obtêm o resultado final das operações com dois conjuntos, no formato LaTeX
+    public String getResultadoOperacaoAB(String nomeA, String nomeB, String operacao, String conjuntoResultante) {
+        return "$$" + " \\ " + nomeA + operacao + " \\ " + nomeB + " = " + BRACE_LEFT + conjuntoResultante + BRACE_RIGHT + "$$";
+    }
+
+    public String convert(TextInputLayout textInputLayout){
+        return textInputLayout.getEditText().getText().toString();
+    }
+
+    public String imprimirConjuntosLatex(String conjuntoA, String conjuntoB, String conjuntoU) {
+
+        String resultado = "";
+        String[][] conjuntos = {{"A", ""},{"B", ""},{"U", ""}};
+
+        String titulo = "$$\\normalsize \\bold{Conjuntos}$$";
+
+        conjuntos[0][1] = conjuntoA;
+        conjuntos[1][1] = conjuntoB;
+        conjuntos[2][1] = conjuntoU;
+
+        for (int atual = 0; atual < conjuntos.length; atual++) {
+            if (conjuntos[atual][INDEX_VALUES] != "") {
+                resultado += "$$";
+                resultado += conjuntos[atual][INDEX_ID];
+                resultado += " = ";
+                resultado += BRACE_LEFT;
+                resultado += conjuntos[atual][INDEX_VALUES];
+                resultado += BRACE_RIGHT;
+                resultado += "$$";
+            }
+        }
+
+        return titulo + resultado + "$$ \\ $$";
     }
 
 
